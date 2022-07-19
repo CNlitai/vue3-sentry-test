20b4b72ad82225dd8a81591e15d80be3ac0ab0f7
CN-litai <2083645366@qq.com>
2022-07-19 18:21:42 +0800

    fix

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 /*
     @param {string} str - string to be converted
  */
+// 更新package.json文件的版本号
+import inquirer from "inquirer";
+import { exec } from "child_process";
+function index() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'version',
+      message: '🚀🚀 请选择更新类型',
+      choices: [
+        { name: '补丁(patch)', value: 'patch' },
+        { name: '次要(minor)', value: 'minor' },
+        { name: '主要(major)', value: 'major' },
+      ],
+    }
+  ]).then(answers => {
+    console.log(JSON.stringify(answers));
+    updatePackageJson(JSON.stringify(answers.version));
+  });
+}
+
+// 更新package.json文件的版本号
+function updatePackageJson(version) {
+  var cmd = `yarn version --${version} --no-git-tag-version`;
+  exec(cmd, function (err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(stdout);
+    prePublish();
+  });
+}
+
 // 将本次commit插入上一条commit消息中
 function prePublish() {
-    var exec = require('child_process').exec;
     var cmd = 'git log -1 --pretty=%B';
     exec(cmd, function (err, stdout, stderr) {
         if (err) {
@@ -22,4 +55,4 @@ function prePublish() {
         });
     });
 }
-prePublish()
+index();
